4d1ed31631cf01874bb810c5f597eadcf4492445
Soumyajit Paria <soumyajitparia2005@gmail.com>
2022-12-11 17:10:30 +0530

    Add SumOfArray

---
+++ largestElementOfArray.py
@@ -0,0 +1,5 @@
+# Input : arr[] = {10, 20, 4}
+# Output : 20
+
+# Input : arr[] = {20, 10, 20, 4, 100}
+# Output : 100
---
+++ sumOfArray.py
@@ -0,0 +1,27 @@
+# Input : arr[] = {1, 2, 3}
+# Output : 6
+# 1 + 2 + 3 = 6
+
+# Input : arr[] = {15, 12, 13, 10}
+# Output : 50
+
+# Python 3 code to find sum
+# of elements in given array
+
+def sum(arr):
+    sum = 0;
+    for items in arr:
+        sum += items
+
+    return sum
+
+# input values to list
+arr = [12, 3, 4, 15]
+ 
+# calculating length of array
+n = len(arr)
+ 
+ans = sum(arr)
+ 
+# display sum
+print('Sum of the array is ', ans)
