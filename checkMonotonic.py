d83d024b8be7807114e02424c352698216de7b57
Soumyajit Paria <soumyajitparia2005@gmail.com>
2022-12-12 14:43:01 +0530

    Added find remainder

---
+++ checkMonotonic.py
@@ -0,0 +1,14 @@
+# Given an array A containing n integers. The task is to check whether the array is Monotonic or not. An array is monotonic if it is either monotone increasing or monotone decreasing. An array A is monotone increasing if for all i <= j, A[i] <= A[j]. 
+
+# An array A is monotone decreasing if for all i <= j, A[i] >= A[j]. 
+
+# Return Type: Boolean value, “True” if the given array A is monotonic else return “False” (without quotes). 
+
+def isMonotonic(A):
+    pass
+
+# Driver program
+A = [6, 5, 4, 4]
+ 
+# Print required result
+print(isMonotonic(A))
---
+++ findRemainder.py
@@ -0,0 +1,13 @@
+# Input : arr[] = {100, 10, 5, 25, 35, 14}, n = 11
+# Output : 9
+# Explanation: 100 x 10 x 5 x 25 x 35 x 14 = 61250000 % 11 = 9
+
+def find_remainder(arr, n):
+    temp = 1
+    for number in arr:
+        temp = number * temp
+    print(temp%n)
+
+arr = [100, 10, 5, 25, 35, 14]
+n = 11
+find_remainder(arr, n) 
