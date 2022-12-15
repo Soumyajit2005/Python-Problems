b736770349a42c85fda06f99ae1db74ba56e2002
Soumyajit Paria <soumyajitparia2005@gmail.com>
2022-12-15 20:47:47 +0530

    Wrote subbarraygivensum

--- subArrWgivenSum.py
+++ subArrWgivenSum.py
@@ -7,16 +7,21 @@ class Solution:
     def subArraySum(self,arr, n, s):
         sum = 0
         for i in range(n):
-            if arr[i] == s:
+            sum = arr[i]
+            if sum == s:
                 return "The sum found at index " + str(i)
             else:
                 for j in range(i+1,n):
+                    sum += arr[j]
                     if sum == s:
-                        return "The sum found between indexes",i,j
-                    sum += arr[i]
+                        return "The sum found between indexes",i+1,j+1
+        return "No sub array found"
+                    
 
 ob = Solution()
-ob.subArraySum([1,2,3,7,5], 5, 12) #Output: 2, 4
+print(ob.subArraySum([1,2,3,7,5], 5, 12)) #Output: 2, 4
 
-
-#Wrong code will fix it later
+arr = [15,2,4,8,9,5,10,23]
+n = len(arr)
+sum = 23
+print(ob.subArraySum(arr, n, sum))  #Output: 2, 5
